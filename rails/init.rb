55427586a7992359cec0d30ba0ddc7dc538207ed
Tobias Schmidt <tobi@gronom.de>
2009-12-05 00:30:30 +0100

    autoload plugin

--- rails/init.rb
+++ rails/init.rb
@@ -1,1 +1,4 @@
 require "sortable_table"
+
+ActionController::Base.send(:include, SortableTable::App::Controllers::ApplicationController)
+ActionView::Base.send(:include, SortableTable::App::Helpers::ApplicationHelper)
